939840e7b86e3f711ea8fba5b545af3b94388dcd
Kamran Azim <Kamazim121212@gmail.com>
2024-04-13 00:39:36 +0100

    Got different game data such as game title, new_price, old price, review score and discount

--- main.py
+++ main.py
@@ -8,9 +8,20 @@ game_data = []
 #GET GAME TITLE
 for d in divs:
     title = d.css_first("div[class *= 'StoreSaleWidgetTitle']").text()
+    new_price = d.css_first("div[class *= 'Wh0L8E']").text()
+    old_price = d.css_first("div[class *= '_1EKG']").text()
+    review_score = d.css_first("div[class *= '_2SbZztpb'] > div").text()
+    
+    discount = d.css_first("div[class *= 'StoreSalePriceWidgetContainer'] > div").text()
+    if discount == "New":
+        discount = d.css_first("div[class *= 'StoreSalePriceWidgetContainer'] > div + div").text()
 
     data = {
-        "title":title
+        "title":title,
+        "new_price":new_price,
+        "old_price":old_price,
+        "review_score":review_score,
+        "discount":discount
     }
 
     game_data.append(data)
--- page_contents.py
+++ page_contents.py
@@ -5,18 +5,20 @@ from time import sleep
 def get_html():
     with sync_playwright() as playwright:
         # SET UP PAGE
-        browser = playwright.chromium.launch(headless=True)
+        browser = playwright.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto("https://store.steampowered.com/specials/")
 
         # WAIT FOR PAGE TO LOAD
         page.wait_for_load_state("networkidle")
         page.mouse.wheel(0,4000)
-        page.wait_for_selector("div[class *= 'NO-IPp'] > div")
+        page.wait_for_selector("div[class *= 'NO-IP'] > div[class *= 'y9MSdl']")
+        print("Got Selector")
         sleep(1)
 
         # GET HTML
         html = page.inner_html("body")
         tree = HTMLParser(html)
-        divs = tree.css("div[class *= 'NO-IPp'] > div")
-        return divs[0:-2]
+        divs = tree.css("div[class *= 'NO-IP'] > div[class *= 'y9MSdl']")
+        return divs
+    
